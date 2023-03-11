508aabd2f80d41bf834a74754c19e698de555230
MichalSerdynski <michalserdynski@icloud.com>
2023-03-11 16:56:01 -0500

    sellStocks method is fully functional
    - user can sell stocks bought by them, based on their price and current price of the stock
    
    - users stocks are stored in a hashmap

--- src/InvestmentCenter.java
+++ src/InvestmentCenter.java
@@ -1,9 +1,55 @@
 import java.util.*;
 
 public class InvestmentCenter {
-    public static double stockPrice;
+    public static Hashtable<String, Integer> ownedStocks = new Hashtable<>();
     public static Hashtable<String, Double> stocks = new Hashtable<>();
     static Scanner scan = new Scanner(System.in);
+
+    public static void sellStocks(){
+        System.out.println("Owned Stocks: ");
+        if(ownedStocks.isEmpty()){
+            System.out.println("You don't own any stocks.");
+            return;
+        }
+        for (Map.Entry<String, Integer> e : ownedStocks.entrySet()) {
+            System.out.println(" | " + e.getKey() + " | " + e.getValue() + " | ");
+        }
+        System.out.println("\n");
+
+        System.out.println("Which stock would you like to sell?");
+        String stockToSell = scan.next();
+
+        if(!ownedStocks.containsKey(stockToSell)){
+            System.out.println("You don't own " + stockToSell + " stocks.");
+            return;
+        }
+
+        double stockPrice = stocks.get(stockToSell);
+        int numberOfStocks = ownedStocks.get(stockToSell);
+
+        System.out.println("You own " + numberOfStocks + " " + stockToSell + " stocks, and the current price is " + stockPrice + " USD.");
+        System.out.println("What is the selling price per stock?");
+        double sellingPrice = scan.nextDouble();
+
+        if(sellingPrice < stockPrice){
+            System.out.println("The selling price cannot be lower than the current price.");
+            return;
+        }
+
+        double totalPrice = numberOfStocks * sellingPrice;
+        System.out.println("Total price: " + totalPrice + " USD");
+
+        System.out.println("Are you sure you want to sell " + numberOfStocks + " " + stockToSell + " stocks for " + totalPrice + " USD? (yes/no)");
+        String answer = scan.next();
+
+        if(answer.equalsIgnoreCase("yes")){
+            Account.availableFunds += totalPrice;
+            ownedStocks.remove(stockToSell);
+            System.out.println("Transaction successful. " + numberOfStocks + " " + stockToSell + " stocks sold for " + totalPrice + " USD.");
+        }else{
+            System.out.println("Transaction cancelled.");
+        }
+    }
 
     public static double totalPrice(double numberOfStocks, double stockPrice) {
         double price = numberOfStocks * stockPrice;
@@ -41,8 +87,8 @@ public class InvestmentCenter {
             switch (choice) {
                 case 1:
                     System.out.println("Number of stocks to buy: ");
-                    int number = scan.nextInt();
-                    double price = totalPrice(t, number);
+                    int numberTSLA = scan.nextInt();
+                    double price = totalPrice(t, numberTSLA);
                     System.out.println("Price: " + price + " USD" + "\n");
                     System.out.println("Accept the transaction ? \n");
                     System.out.println("Type 'yes' to proceed, and any other key to cancel");
@@ -50,6 +96,7 @@ public class InvestmentCenter {
                     if (accept.equalsIgnoreCase("yes")) {
                         if (Account.availableFunds >= price) {
                             Account.availableFunds -= price;
+                            ownedStocks.put("TSLA", numberTSLA);
                             System.out.println("Transaction Successful");
                         } else {
                             System.out.println("Not enough funds");
@@ -69,6 +116,7 @@ public class InvestmentCenter {
                     if (acceptNVDA.equalsIgnoreCase("yes")) {
                         if (Account.availableFunds >= priceNVDA) {
                             Account.availableFunds -= priceNVDA;
+                            ownedStocks.put("NVDA", numberNVDA);
                             System.out.println("Transaction Successful");
                         } else {
                             System.out.println("Not enough funds");
@@ -88,6 +136,7 @@ public class InvestmentCenter {
                     if (acceptAAPL.equalsIgnoreCase("yes")) {
                         if (Account.availableFunds >= priceAAPL) {
                             Account.availableFunds -= priceAAPL;
+                            ownedStocks.put("AAPL", numberAAPL);
                             System.out.println("Transaction Successful");
                         } else {
                             System.out.println("Not enough funds");
@@ -107,6 +156,7 @@ public class InvestmentCenter {
                     if (acceptMSFT.equalsIgnoreCase("yes")) {
                         if (Account.availableFunds >= priceMSFT) {
                             Account.availableFunds -= priceMSFT;
+                            ownedStocks.put("MSFT", numberMSFT);
                             System.out.println("Transaction Successful");
                         } else {
                             System.out.println("Not enough funds");
@@ -126,6 +176,7 @@ public class InvestmentCenter {
                     if (acceptAMZN.equalsIgnoreCase("yes")) {
                         if (Account.availableFunds >= priceAMZN) {
                             Account.availableFunds -= priceAMZN;
+                            ownedStocks.put("AMZN", numberAMZN);
                             System.out.println("Transaction Successful");
                         } else {
                             System.out.println("Not enough funds");
@@ -170,7 +221,7 @@ public class InvestmentCenter {
             System.out.println("1. Transfer Funds");
             System.out.println("2. Withdraw Funds");
             System.out.println("3. Invest");
-            System.out.println("4. Check Investments");
+            System.out.println("4. Sell Stocks");
             System.out.println("5. Exit");
             System.out.println("\n");
             System.out.println("Type the number of chosen option to access it");
@@ -190,10 +241,10 @@ public class InvestmentCenter {
                     withdrawFunds(withdraw);
                     break;
                 case 3:
-
-                        marketSituation();
-
-                            break;
+                    marketSituation();
+                    break;
+                case 4:
+                    sellStocks();
 
 
                // case 4:
